97d5039fdc293cc7543c192730ee4be6d2a8fced
madmax0693-ui <madmax0693@gmail.com>
2026-06-06 14:23:31 +0300

    Delete Новая папка (2) directory

--- Новая папка (2)/sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='tumorboard-urology-v1';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./icon.svg'];
-self.addEventListener('install', e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch', e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
